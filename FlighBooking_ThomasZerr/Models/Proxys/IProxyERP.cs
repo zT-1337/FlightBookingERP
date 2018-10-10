f8d37f462cfdd408a764e133ee50dc50aaa9f5ad
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-10 15:59:29 +0200

    Schnittstellen definiert

---
+++ FlighBooking_ThomasZerr/Models/Proxys/IProxyERP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.FlightBookings;
+
+namespace FlighBooking_ThomasZerr.Models.Proxys
+{
+    interface IProxyERP
+    {
+        string Username { get; set; }
+        string Password { set; }
+
+        ProxyResponseERP FlightBookingConfirm(FlightBookingData args);
+        ProxyResponseERP FlightBookingCancel(FlightBookingData args);
+        ProxyResponseERP FlightBookingCreateFromData(FlightBookingData args);
+        ProxyResponseERP FlightBookingGetList(FlightBookingData args);
+    }
+}
