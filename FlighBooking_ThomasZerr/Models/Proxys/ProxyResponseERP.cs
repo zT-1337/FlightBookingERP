b774d62fd6613119018a7b5ac4303454083db1cb
Thomas Zerr <thomas.zerr@googlemail.com>
2018-10-11 15:30:32 +0200

    Propertys für FlightBookingData und ReturnCode definiert

--- FlighBooking_ThomasZerr/Models/Proxys/ProxyResponseERP.cs
+++ FlighBooking_ThomasZerr/Models/Proxys/ProxyResponseERP.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.FlightBookings;
 
 namespace FlighBooking_ThomasZerr.Models.Proxys
 {
     class ProxyResponseERP
     {
+        public FlightBookingData FlightBookingData { get; set; }
+        public ReturnCodeERP ReturnCode { get; set; }
     }
 }
