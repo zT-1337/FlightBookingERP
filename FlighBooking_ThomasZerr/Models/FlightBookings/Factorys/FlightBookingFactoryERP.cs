88ad0bb144eb95b7ebf39e363afc876f03159167
Thomas Zerr <thomas.zerr@googlemail.com>
2018-10-11 16:25:54 +0200

    Create/RetrieveAll Methoden zum Teil implementiert

--- FlighBooking_ThomasZerr/Models/FlightBookings/Factorys/FlightBookingFactoryERP.cs
+++ FlighBooking_ThomasZerr/Models/FlightBookings/Factorys/FlightBookingFactoryERP.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.Proxys;
 
 namespace FlighBooking_ThomasZerr.Models.FlightBookings.Factorys
 {
-    class FlightBookingFactoryERP
+    class FlightBookingFactoryERP : IFlightBookingFactory
     {
+        private IProxyERP proxyERP_;
+
+        public FlightBookingFactoryERP(IProxyERP proxyERP)
+        {
+            proxyERP_ = proxyERP;
+        }
+
+        public IFlightBooking Create(FlightBookingData args)
+        {
+            ProxyResponseERP response = proxyERP_.FlightBookingCreateFromData(args);
+
+            //TODO FlightBookingERP Objekt erstellen und zurückliefern
+
+            throw new NotImplementedException();
+        }
+
+        public IFlightBooking[] RetrieveAll(FlightBookingData args)
+        {
+            ProxyResponseERP responses = proxyERP_.FlightBookingGetList(args);
+
+            //TODO FlightBookingERP Objekte erstellen und zurückliefern
+
+            throw new NotImplementedException();
+        }
     }
 }
