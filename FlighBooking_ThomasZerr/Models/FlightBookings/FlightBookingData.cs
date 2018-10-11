2224dc7c19384782f54e5b1faac36385cd84d845
Thomas Zerr <thomas.zerr@googlemail.com>
2018-10-11 16:09:46 +0200

    weiter propertys hinzugefügt

--- FlighBooking_ThomasZerr/Models/FlightBookings/FlightBookingData.cs
+++ FlighBooking_ThomasZerr/Models/FlightBookings/FlightBookingData.cs
@@ -8,9 +8,16 @@ namespace FlighBooking_ThomasZerr.Models.FlightBookings
 {
     class FlightBookingData
     {
+        //TODO ist Airline und AirlineId das gleiche?
         public string AirlineId { get; set; }
+        public string Airline { get; set; }
         public string BookingNumber { get; set; }
         public string TestRun { get; set; }
+        public string ReserveOnly { get; set; }
+        public string CustomerNumber { get; set; }
+        public int MaxRows { get; set; }
+        public bool MaxRowsSpecified { get; set; }
+        public string TravelAgency { get; set; }
 
     }
 }
