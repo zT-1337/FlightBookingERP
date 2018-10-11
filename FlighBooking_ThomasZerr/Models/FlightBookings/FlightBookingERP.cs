1ec435540b34f32c9e903f36e90b3c279fa24951
Thomas Zerr <thomas.zerr@googlemail.com>
2018-10-11 16:18:26 +0200

    Confirm/Cancel zum Teil implementiert

--- FlighBooking_ThomasZerr/Models/FlightBookings/FlightBookingERP.cs
+++ FlighBooking_ThomasZerr/Models/FlightBookings/FlightBookingERP.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.Proxys;
 
 namespace FlighBooking_ThomasZerr.Models.FlightBookings
 {
-    class FlightBookingERP
+    class FlightBookingERP : IFlightBooking
     {
+        private IProxyERP proxyERP_;
+
+        public FlightBookingData FlightBookingData { get; set; }
+
+        public FlightBookingERP(IProxyERP proxyERP)
+        {
+            proxyERP_ = proxyERP;
+        }
+
+        public void Confirm()
+        {
+            ProxyResponseERP response = proxyERP_.FlightBookingConfirm(FlightBookingData);
+
+            //TODO response verarbeiten
+
+            throw new NotImplementedException();
+        }
+
+        public void Cancel()
+        {
+            ProxyResponseERP response = proxyERP_.FlightBookingCancel(FlightBookingData);
+
+            //TODO response verarbeiten
+
+            throw new NotImplementedException();
+        }
     }
 }
