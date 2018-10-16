d971c4eb577bc35754bad2a4ff24eea96bacb33d
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-16 16:31:29 +0200

    propertys hinzugefügt

--- FlighBooking_ThomasZerr/Models/FlightBookings/FlightBookingDatas/FlightBookingData.cs
+++ FlighBooking_ThomasZerr/Models/FlightBookings/FlightBookingDatas/FlightBookingData.cs
@@ -4,14 +4,28 @@ namespace FlighBooking_ThomasZerr.Models.FlightBookings.FlightBookingDatas
 {
     class FlightBookingData
     {
+        //TODO Eventuell in bessere Datentypen umwandeln
         public string AirlineId { get; set; }
         public string BookingId { get; set; }
+        public string ConnectId { get; set; }
+        public string Flightdate { get; set; }
+        public string CustomerId { get; set; }
+        public string Class { get; set; }
+        public string Bookdate { get; set; }
+        public string Counter { get; set; }
+        public string AgencyId { get; set; }
+        public string PassagierName { get; set; }
+        //TODO in bool ändern
+        public string Reserved { get; set; }
+        public string Cancelled { get; set; }
+
         public string Airline { get; set; }
         public string BookingNumber { get; set; }
         public string TestRun { get; set; }
         public string ReserveOnly { get; set; }
         public string CustomerNumber { get; set; }
         public string TravelAgency { get; set; }
+        //TODO aus dieser Klasse schmeißen
         public DateRange BookingDateRange { get; set; }
         public DateRange FlightDateRange { get; set; }
 
