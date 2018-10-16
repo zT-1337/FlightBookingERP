860e74c5a1b0be95fd51965284c0e99b68eb93b5
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-16 15:24:43 +0200

    Options und IsRangeIncluded propertys hinzugefügt

--- FlighBooking_ThomasZerr/Models/FlightBookings/FlightBookingDatas/DateRange.cs
+++ FlighBooking_ThomasZerr/Models/FlightBookings/FlightBookingDatas/DateRange.cs
@@ -8,5 +8,9 @@ namespace FlighBooking_ThomasZerr.Models.FlightBookings.FlightBookingDatas
 {
     class DateRange
     {
+        public string EarlierDate { get; set; }
+        public string LaterDate { get; set; }
+        public DateRangeOptions Options { get; set; }
+        public bool IsRangeIncluded { get; set; }
     }
 }
