66cfd7057996b63044658a06737c9788dfd1e443
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-17 17:09:32 +0200

    Propertys und Methoden für GetList hinzugefügt

--- FlighBooking_ThomasZerr/ViewModels/FlightBookingViewModels/IFlightBookingViewModel.cs
+++ FlighBooking_ThomasZerr/ViewModels/FlightBookingViewModels/IFlightBookingViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FlighBooking_ThomasZerr.Models.FlightBookings;
+using FlighBooking_ThomasZerr.Models.FlightBookings.FlightBookingDatas.DateRanges;
 
 namespace FlighBooking_ThomasZerr.ViewModels.FlightBookingViewModels
 {
@@ -12,5 +13,13 @@ namespace FlighBooking_ThomasZerr.ViewModels.FlightBookingViewModels
     {
         IFlightBooking ChosenFlightBooking { get; set; }
         ObservableCollection<IFlightBooking> RetrievedFlightBookings { get; }
+
+        string AirlineName { get; set; }
+        string TravelAgency { get; set; }
+        string CustomerNumber { get; set; }
+        DateRange BookingDateRange { get; set; }
+        DateRange FlightDateRange { get; set; }
+
+        void DoFlightBookingSearch();
     }
 }
