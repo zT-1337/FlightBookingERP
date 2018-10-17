687c2dfaabfae28684ae0d99875a0e0eeab6f9e1
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-17 17:29:39 +0200

    Interface implementiert

--- FlighBooking_ThomasZerr/ViewModels/FlightBookingViewModels/FlightBookingViewModelImpl.cs
+++ FlighBooking_ThomasZerr/ViewModels/FlightBookingViewModels/FlightBookingViewModelImpl.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.FlightBookings;
+using FlighBooking_ThomasZerr.Models.FlightBookings.Factorys;
+using FlighBooking_ThomasZerr.Models.FlightBookings.FlightBookingDatas.DateRanges;
 
 namespace FlighBooking_ThomasZerr.ViewModels.FlightBookingViewModels
 {
-    class FlightBookingViewModelImpl
+    class FlightBookingViewModelImpl : IFlightBookingViewModel
     {
+        private IFlightBookingFactory flightBookingFactory_;
+
+        public IFlightBooking ChosenFlightBooking { get; }
+        public ObservableCollection<IFlightBooking> RetrievedFlightBookings { get; }
+
+        public string AirlineName { get; set; }
+        public string TravelAgency { get; set; }
+        public string CustomerNumber { get; set; }
+        public IDateRange BookingDateRange { get; }
+        public IDateRange FlightDateRange { get; }
+
+        public void DoFlightBookingSearch()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
