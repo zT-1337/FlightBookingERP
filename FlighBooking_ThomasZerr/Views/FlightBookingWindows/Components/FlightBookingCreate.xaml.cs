114b9dfb50622a7e93ca6f6830f8d0f1e9f4310e
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-23 18:31:07 +0200

    Create methode für button implementiert

---
+++ FlighBooking_ThomasZerr/Views/FlightBookingWindows/Components/FlightBookingCreate.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FlighBooking_ThomasZerr.ViewModels.FlightBookingViewModels;
+
+namespace FlighBooking_ThomasZerr.Views.FlightBookingWindows.Components
+{
+    /// <summary>
+    /// Interaktionslogik für FlightBookingCreate.xaml
+    /// </summary>
+    public partial class FlightBookingCreate : UserControl
+    {
+        public FlightBookingCreate()
+        {
+            InitializeComponent();
+        }
+
+        private void DoCreate(object sender, RoutedEventArgs e)
+        {
+            var flightBookingViewModel = (IFlightBookingViewModel) DataContext;
+            flightBookingViewModel.DoCreateFlightBooking();
+        }
+    }
+}
