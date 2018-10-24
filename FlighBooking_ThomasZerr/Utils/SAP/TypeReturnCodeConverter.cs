b385dd7c00513f91dbcc53917090b5b94f67ab6f
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-24 19:06:37 +0200

    init commit

---
+++ FlighBooking_ThomasZerr/Utils/SAP/TypeReturnCodeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.Proxys;
+
+namespace FlighBooking_ThomasZerr.Utils.SAP
+{
+    class TypeReturnCodeConverter
+    {
+        public static ReturnCodeProxys TypeToReturnCode(string type)
+        {
+            switch (type)
+            {
+                case "S":
+                    return ReturnCodeProxys.Success;
+                case "E":
+                    return ReturnCodeProxys.Error;
+                case "W":
+                    return ReturnCodeProxys.Warning;
+                case "I":
+                    return ReturnCodeProxys.Information;
+                case "A":
+                    return ReturnCodeProxys.Abort;
+            }
+
+            throw new InvalidOperationException($"Gegebener Type unbekannt: {type}");
+        }
+    }
+}
