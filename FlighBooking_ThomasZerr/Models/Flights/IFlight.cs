3a1e94e7278d481759ca6646b08ff6631f608f0f
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-24 18:28:10 +0200

    public gemacht

--- FlighBooking_ThomasZerr/Models/Flights/IFlight.cs
+++ FlighBooking_ThomasZerr/Models/Flights/IFlight.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.Flights.FlightDatas;
 
 namespace FlighBooking_ThomasZerr.Models.Flights
 {
-    interface IFlight
+    public interface IFlight
     {
+        IFlightData FlightData { get; }
     }
 }
