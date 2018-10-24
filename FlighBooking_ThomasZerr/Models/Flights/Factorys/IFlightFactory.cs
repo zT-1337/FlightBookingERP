a8d91602bb92ed42381012c2d60c62b88c914976
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-24 18:41:09 +0200

    init commit

---
+++ FlighBooking_ThomasZerr/Models/Flights/Factorys/IFlightFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.Flights.FlightDatas;
+
+namespace FlighBooking_ThomasZerr.Models.Flights.Factorys
+{
+    interface IFlightFactory
+    {
+        IFlight Create(IFlightData data);
+    }
+}
---
+++ FlighBooking_ThomasZerr/Models/Flights/FlightImpl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using FlighBooking_ThomasZerr.Models.Flights.FlightDatas;
+using FlighBooking_ThomasZerr.Models.Proxys;
+using FlighBooking_ThomasZerr.Models.Proxys.FlightProxys;
+
+namespace FlighBooking_ThomasZerr.Models.Flights
+{
+    class FlightImpl : IFlight
+    {
+        private IProxyFlight proxyFlight_;
+
+        public IFlightData FlightData { get; }
+
+        public FlightImpl(IProxyFlight proxyFlight)
+        {
+            proxyFlight_ = proxyFlight;
+        }
+
+        public bool IsExisting()
+        {
+            ProxyFlightResponse proxyFlightResponse = proxyFlight_.IsExisting(FlightData);
+            return proxyFlightResponse.ReturnCode == ReturnCodeProxys.Success;
+        }
+    }
+}
