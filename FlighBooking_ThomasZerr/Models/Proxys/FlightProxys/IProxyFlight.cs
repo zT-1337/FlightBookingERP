8eb8cccb979da5f0d3d0e9349b23a5ccf4325851
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-24 18:19:10 +0200

    Schnittstellen definiert

---
+++ FlighBooking_ThomasZerr/Models/Proxys/FlightProxys/IProxyFlight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.Flights.FlightDatas;
+
+namespace FlighBooking_ThomasZerr.Models.Proxys.FlightProxys
+{
+    interface IProxyFlight
+    {
+        string Username { get; set; }
+        string Password { set; }
+
+        ProxyFlightResponse IsExisting(IFlightData args);
+        ProxyFlightResponse Create(IFlightData args);
+    }
+}
