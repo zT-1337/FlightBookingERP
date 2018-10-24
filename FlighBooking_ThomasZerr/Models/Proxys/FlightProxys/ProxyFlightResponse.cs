2a03e59431c8138500156bb9d1ff8733668e8282
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-24 18:47:52 +0200

    Property FlightData hinzugefügt

--- FlighBooking_ThomasZerr/Models/Proxys/FlightProxys/ProxyFlightResponse.cs
+++ FlighBooking_ThomasZerr/Models/Proxys/FlightProxys/ProxyFlightResponse.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.Flights.FlightDatas;
 
 namespace FlighBooking_ThomasZerr.Models.Proxys.FlightProxys
 {
     class ProxyFlightResponse
     {
+        public IFlightData FlightData { get; set; }
         public ReturnCodeProxys ReturnCode { get; set; }
         public string Message { get; set; }
     }
