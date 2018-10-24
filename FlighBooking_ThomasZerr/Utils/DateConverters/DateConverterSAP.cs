4c19b31facba9bc7b935a400f06a81c6777a3b8b
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-24 16:24:22 +0200

    DateKonvertierung in eigene klasse gepackt

---
+++ FlighBooking_ThomasZerr/Utils/DateConverters/DateConverterSAP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlighBooking_ThomasZerr.Utils.DateConverters
+{
+    class DateConverterSAP : IDateConverter
+    {
+        public string ConvertDateTimeToString(DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd");
+        }
+
+        public DateTime ConvertStringToDateTime(string date)
+        {
+            int year = Int32.Parse(date.Substring(0, 4));
+            int month = Int32.Parse(date.Substring(4, 2));
+            int day = Int32.Parse(date.Substring(6, 2));
+
+            return new DateTime(year, month, day);
+        }
+    }
+}
---
+++ FlighBooking_ThomasZerr/Utils/DateConverters/IDateConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlighBooking_ThomasZerr.Utils.DateConverters
+{
+    interface IDateConverter
+    {
+        string ConvertDateTimeToString(DateTime dateTime);
+        DateTime ConvertStringToDateTime(string date);
+    }
+}
