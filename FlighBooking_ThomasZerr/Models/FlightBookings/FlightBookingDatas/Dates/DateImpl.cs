72d233b636bba52b25fa056cbf6040cdc1bf1a27
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-24 16:27:38 +0200

    init commit

---
+++ FlighBooking_ThomasZerr/Models/FlightBookings/FlightBookingDatas/Dates/DateImpl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Utils.DateConverters;
+
+namespace FlighBooking_ThomasZerr.Models.FlightBookings.FlightBookingDatas.Dates
+{
+    class DateImpl : IDate
+    {
+        private IDateConverter dateConverter_;
+
+        public DateTime Date { get; set; }
+        public string DateString
+        {
+            get => dateConverter_.ConvertDateTimeToString(Date);
+            set => Date = dateConverter_.ConvertStringToDateTime(value);
+        }
+
+        public DateImpl(IDateConverter dateConverter)
+        {
+            dateConverter_ = dateConverter;
+        }
+    }
+}
---
+++ FlighBooking_ThomasZerr/Models/FlightBookings/FlightBookingDatas/Dates/IDate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlighBooking_ThomasZerr.Models.FlightBookings.FlightBookingDatas.Dates
+{
+    interface IDate
+    {
+        DateTime Date { get; set; }
+        string DateString { get; set; }
+    }
+}
