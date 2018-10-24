b74c2a624676b70c79c0512abe470b81b5d6f288
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-24 18:27:48 +0200

    propertys hinzugefügt

--- FlighBooking_ThomasZerr/Models/Flights/FlightDatas/IFlightData.cs
+++ FlighBooking_ThomasZerr/Models/Flights/FlightDatas/IFlightData.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.FlightBookings.FlightBookingDatas.Dates;
 
 namespace FlighBooking_ThomasZerr.Models.Flights.FlightDatas
 {
-    interface IFlightData
+    public interface IFlightData
     {
+        string AirlineId { get; set; }
+        string ConnectId { get; set; }
+        IDate Flightdate { get; }
+        string Planetype { get; set; }
+        string CurrencyIso { get; set; }
     }
 }
