5411f0710f7e48f343cf1bb91032abb070baf6b1
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-24 19:07:24 +0200

    Implementiert

---
+++ FlighBooking_ThomasZerr/Models/Proxys/FlightProxys/ProxyFlightSAP.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Flight;
+using FlighBooking_ThomasZerr.Models.Flights.FlightDatas;
+using FlighBooking_ThomasZerr.Utils.SAP;
+
+namespace FlighBooking_ThomasZerr.Models.Proxys.FlightProxys
+{
+    class ProxyFlightSAP : IProxyFlight
+    {
+        private Z_FLIGHT_MTClient sapClient_;
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public ProxyFlightSAP()
+        {
+            sapClient_ = new Z_FLIGHT_MTClient();
+        }
+
+        public ProxyFlightResponse IsExisting(IFlightData args)
+        {
+            var flightGetDetail = new FlightGetDetail
+            {
+                AirlineID = args.AirlineId,
+                ConnectionID = args.ConnectId,
+                FlightDate = args.Flightdate.DateString
+            };
+
+            var sapResponse = sapClient_.FlightGetDetail(flightGetDetail);
+
+            ReturnCodeProxys returnCode = TypeReturnCodeConverter.TypeToReturnCode(sapResponse.Return[0].Type);
+            string message = sapResponse.Return[0].Message;
+
+            return new ProxyFlightResponse
+            {
+                ReturnCode =  returnCode,
+                Message = message
+            };
+        }
+
+        public ProxyFlightResponse Create(IFlightData args)
+        {
+            Bapisflrep flightData = ConvertFlightDataToBapisflrep(args);
+
+            var flightCreate = new FlightSaveReplica()
+            {
+                FlightData = flightData
+            };
+
+            var sapResponse = sapClient_.FlightSaveReplica(flightCreate);
+
+            ReturnCodeProxys returnCode = TypeReturnCodeConverter.TypeToReturnCode(sapResponse.Return[0].Type);
+            string message = sapResponse.Return[0].Message;
+
+            return new ProxyFlightResponse
+            {
+                ReturnCode = returnCode,
+                Message = message
+            };
+        }
+
+        private Bapisflrep ConvertFlightDataToBapisflrep(IFlightData args)
+        {
+            return new Bapisflrep
+            {
+                Airlineid = args.AirlineId,
+                Connectid = args.ConnectId,
+                CurrIso = args.CurrencyIso,
+                Flightdate = args.Flightdate.DateString,
+                Planetype = args.Planetype,
+                Price = args.Price
+            };
+        }
+    }
+}
