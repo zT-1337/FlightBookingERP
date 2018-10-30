4c48c1ce9b4c873efe67fb498f79bbc1c84e058b
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-30 19:14:26 +0100

    implementiert

--- FlighBooking_ThomasZerr/Views/FlightBookingMainWindows/Factorys/FlightBookingMainWindowFactoryImpl.cs
+++ FlighBooking_ThomasZerr/Views/FlightBookingMainWindows/Factorys/FlightBookingMainWindowFactoryImpl.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.ObjectModel;
 using System.Configuration;
-using FlighBooking_ThomasZerr.Models.FlightBookings;
-using FlighBooking_ThomasZerr.Models.FlightBookings.Factorys;
-using FlighBooking_ThomasZerr.Models.FlightBookings.FlightBookingDatas;
-using FlighBooking_ThomasZerr.Models.FlightBookings.FlightBookingDatas.DateRanges;
-using FlighBooking_ThomasZerr.Models.Flights.Factorys;
-using FlighBooking_ThomasZerr.Models.Proxys.FlightBookingProxys;
-using FlighBooking_ThomasZerr.Models.Proxys.FlightProxys;
-using FlighBooking_ThomasZerr.ViewModels.FlightBookingViewModels;
+using FlighBooking_ThomasZerr.Views.FlightBookingCreateWindows.Factorys;
+using FlighBooking_ThomasZerr.Views.FlightBookingEditWindows.Factorys;
 
 namespace FlighBooking_ThomasZerr.Views.FlightBookingMainWindows.Factorys
 {
@@ -26,7 +19,19 @@ namespace FlighBooking_ThomasZerr.Views.FlightBookingMainWindows.Factorys
 
         private FlightBookingMainWindow CreateForSAP(string username, string password)
         {
-            throw new NotImplementedException();
+            var createWindowFactory = new FlightBookingCreateWindowFactorySAP
+            {
+                Username = username,
+                Password = password
+                
+            };
+            var editWindowFactory = new FlightBookingEditWindowFactorySAP
+            {
+                Username = username,
+                Password = password
+            };
+
+            return new FlightBookingMainWindow(createWindowFactory, editWindowFactory);
         }
     }
 }
