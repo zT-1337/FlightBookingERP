9e392ea6610b9572920d6305485d0cfe5ed0cf3d
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-10-30 19:04:21 +0100

    implementiert

---
+++ FlighBooking_ThomasZerr/Views/FlightBookingEditWindows/Factorys/FlightBookingEditWindowFactorySAP.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.FlightBookings;
+using FlighBooking_ThomasZerr.Models.FlightBookings.Factorys;
+using FlighBooking_ThomasZerr.Models.FlightBookings.FlightBookingDatas;
+using FlighBooking_ThomasZerr.Models.Proxys.FlightBookingProxys;
+using FlighBooking_ThomasZerr.ViewModels.FlightBookingEditViewModels;
+
+namespace FlighBooking_ThomasZerr.Views.FlightBookingEditWindows.Factorys
+{
+    class FlightBookingEditWindowFactorySAP : IFlightBookingEditWindowFactory
+    {
+        public FlightBookingEditWindow Create(string username, string password)
+        {
+            var editViewModel = CreateViewModel(username, password);
+            return new FlightBookingEditWindow(editViewModel);
+        }
+
+        private IFlightBookingEditViewModel CreateViewModel(string username, string password)
+        {
+            var flightBookingFactory = CreateFlightBookingFactory(username, password);
+            var defaultFlightBookingData = CreateDefaultFlightBookingArgs();
+
+            return new FlightBookingEditViewModelImpl(flightBookingFactory, 
+                defaultFlightBookingData, 
+                new ObservableCollection<IFlightBooking>());
+        }
+
+        private IFlightBookingFactory CreateFlightBookingFactory(string username, string password)
+        {
+            var proxyFlightBooking = new ProxyFlightBookingSAP
+            {
+                Username = username,
+                Password = password
+            };
+
+            return new FlightBookingFactoryImpl(proxyFlightBooking);
+        }
+
+        private IFlightBookingData CreateDefaultFlightBookingArgs()
+        {
+            return new FlightBookingDataSAP
+            {
+                BookingId = "",
+                CustomerId = "",
+                Class = "",
+                Counter = "",
+                AgencyId = "",
+                PassagierName = "",
+                Reserved = false,
+                Cancelled = false,
+            };
+        }
+    }
+}
---
+++ FlighBooking_ThomasZerr/Views/FlightBookingEditWindows/Factorys/IFlightBookingEditWindowFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlighBooking_ThomasZerr.Views.FlightBookingEditWindows.Factorys
+{
+    public interface IFlightBookingEditWindowFactory
+    {
+        FlightBookingEditWindow Create(string username, string password);
+    }
+}
