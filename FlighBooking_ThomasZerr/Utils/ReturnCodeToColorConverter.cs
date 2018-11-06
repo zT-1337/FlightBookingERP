d6e45f9c0349b24c1953809cf63ca20c4f07862c
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-11-06 15:12:43 +0100

    init commit

---
+++ FlighBooking_ThomasZerr/Utils/ReturnCodeToColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using FlighBooking_ThomasZerr.Models.OperationResult.ReturnCodes;
+
+namespace FlighBooking_ThomasZerr.Utils
+{
+    class ReturnCodeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ReturnCode returnCode = (ReturnCode) value;
+
+            switch (returnCode)
+            {
+                case ReturnCode.Success:
+                    return Brushes.Green;
+                case ReturnCode.Exception:
+                    return Brushes.Red;
+            }
+
+            throw new ArgumentException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
