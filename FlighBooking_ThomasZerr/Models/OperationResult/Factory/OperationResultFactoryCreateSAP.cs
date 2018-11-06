173d1cdbb139b587a6de7d5b7a1287b184a25101
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-11-06 15:57:26 +0100

    Eigene OperationResultFactory für Create

---
+++ FlighBooking_ThomasZerr/Models/OperationResult/Factory/OperationResultFactoryCreateSAP.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.OperationResult.ReturnCodes;
+
+namespace FlighBooking_ThomasZerr.Models.OperationResult.Factory
+{
+    class OperationResultFactoryCreateSAP : IOperationResultFactory
+    {
+        private IOperationResult successResult_;
+
+        public OperationResultFactoryCreateSAP()
+        {
+            successResult_ = new OperationResultImpl("Operation erfolgreich ausgeführt", ReturnCode.Success);
+        }
+
+        public IOperationResult CreateSuccess()
+        {
+            return successResult_;
+        }
+
+        public IOperationResult CreateException(Exception exception)
+        {
+            string message = CreateMessageForException(exception);
+
+            return new OperationResultImpl(message, ReturnCode.Exception);
+        }
+
+        private string CreateMessageForException(Exception exception)
+        {
+            switch (exception.Message)
+            {
+                case "Eintrag für den Flug bereits gesperrt (Tabelle SFLIGHT)":
+                    return "Operation wird bereits ausgeführt";
+                default:
+                    return exception.Message;
+            }
+        }
+    }
+}
