0a5652647a58fed51eec74c76eef709f6035cffa
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-11-06 17:13:21 +0100

    init commit

---
+++ FlighBooking_ThomasZerr/Utils/ValidationRules/AirlineIdValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace FlighBooking_ThomasZerr.Utils.ValidationRules
+{
+    class AirlineIdValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string airlineId)
+            {
+                if(airlineId.Length > 3)
+                    return new ValidationResult(false, "Darf maximal drei Zeichen lang sein");
+
+                return new ValidationResult(true, "Darf maximal drei Zeichen lang sein");
+            }
+
+            throw new ArgumentException();
+        }
+    }
+}
