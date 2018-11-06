5bb4f414d780340f8a2e3c316592c198c12f82b5
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-11-06 14:05:49 +0100

    init commit

---
+++ FlighBooking_ThomasZerr/Models/OperationResult/Factory/IOperationResultFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlighBooking_ThomasZerr.Models.OperationResult.Factory
+{
+    interface IOperationResultFactory
+    {
+        IOperationResult CreateSuccess();
+        IOperationResult CreateException(Exception exception);
+    }
+}
