bfab73a975cee0ef9a21162a36fddf5a73e00dc5
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-11-06 14:08:32 +0100

    Methoden implementiert

--- FlighBooking_ThomasZerr/Models/OperationResult/Factory/OperationResultFactorySAP.cs
+++ FlighBooking_ThomasZerr/Models/OperationResult/Factory/OperationResultFactorySAP.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.OperationResult.ReturnCodes;
 
 namespace FlighBooking_ThomasZerr.Models.OperationResult.Factory
 {
-    class OperationResultFactorySAP
+    class OperationResultFactorySAP : IOperationResultFactory
     {
+        public IOperationResult CreateSuccess()
+        {
+            return new OperationResultImpl
+            {
+                Message = "Operation erfolgreich ausgeführt",
+                ReturnCode = ReturnCode.Success
+            };
+        }
+
+        public IOperationResult CreateException(Exception exception)
+        {
+            string message = createMessageForException(exception);
+
+            return new OperationResultImpl
+            {
+                Message = message,
+                ReturnCode = ReturnCode.Exception
+            };
+        }
+
+        private string createMessageForException(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
