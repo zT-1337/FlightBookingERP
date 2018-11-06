97b0e1b645ce6a0cd0deee88e31104c29813a1fa
dp1337\Thomas <thomas.zerr@googlemail.com>
2018-11-06 14:03:51 +0100

    init commit

---
+++ FlighBooking_ThomasZerr/Models/OperationResult/OperationResultImpl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlighBooking_ThomasZerr.Models.OperationResult.ReturnCodes;
+
+namespace FlighBooking_ThomasZerr.Models.OperationResult
+{
+    class OperationResultImpl : IOperationResult
+    {
+        public string Message { get; set; }
+        public ReturnCode ReturnCode { get; set; }
+    }
+}
